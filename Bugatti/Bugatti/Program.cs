fbf171993878bde31e235f462b417f1b80fb235e
Benoît <benoitlagasse@hotmail.com>
2020-04-28 00:22:24 +0200

    updated to do list

--- Bugatti/Bugatti/Program.cs
+++ Bugatti/Bugatti/Program.cs
@@ -18,7 +18,11 @@ namespace Bugatti
     6) Voorzie ook een(basis) client applicatie vanwaaruit je je API aanspreekt.De technologie mag je zelf kiezen(Angular, Ionic, gewoon HTML/Javascript,..),
        voorzie schermen om de functionaliteit van je API te kunnen testen en gebruiken.
     7) Werken met hypermedia links om je API naar de "next level" te tillen levert uiteraard meer punten op.
-    8) .... (wordt nog verder aangevuld)
+    8) Voorzie je client app van een login mogelijkheid aan de hand van OAuth/Open ID 
+    9) Beveilig je API deels met een JWT Token (bepaal zelf welk deel)
+   10) Geef het JWT token vanuit je client (nadat de gebruiker is aangemeld) mee naar je API om toegang te bekomen tot het beveiligde deel.
+   11) Betrek eveneens een 3rd party API die op het internet beschikbaar is bij jouw project. Er zijn enorm veel API's beschikbaar. Neem bij voorkeur een API die goed aansluit bij je project. Roep deze aan vanuit de frontend (client) of vanuit de backend (server).
+   12) Deployment naar google cloud (je google cloud credits zijn beperkt dus contacteer me zeker als dit gelukt is zodat je niet zonder credits valt en het nog niet hebt kunnen tonen dat het gelukt was)
     */
 
     public class Program
